ae3fecc14c5b374fa8e995db3475a36815f92e91
Akshit Sharma <akshits349@gmail.com>
2024-06-17 15:01:38 +0530

    change in some dependency

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,11 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import React from 'react';
-
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
@@ -14,6 +9,4 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
-};
-
-export default Layout;
+}
